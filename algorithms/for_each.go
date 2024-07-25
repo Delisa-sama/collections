721f8581ccc501cba3199144ab5902708a07093d
Egor Dubin <delisa.sama@gmail.com>
2024-07-26 02:02:03 +0300

    Добавить ForEachPtr

--- algorithms/for_each.go
+++ algorithms/for_each.go
@@ -8,8 +8,20 @@ import (
 type forEachFunc[T any] func(T)
 
 // ForEach применяет функцию f ко всем элементам в диапазоне от begin до end.
+// На вход функции f будет копия элемента.
 func ForEach[T any](begin interfaces.ForwardIterator[T], end interfaces.Iterator, f forEachFunc[T]) {
 	for it := begin; !it.Equals(end); it.Next() {
 		f(it.Value())
+	}
+}
+
+// forEachFunc представляет функцию, применяемую к каждому элементу контейнера.
+type forEachPtrFunc[T any] func(*T)
+
+// ForEachPtr применяет функцию f ко всем элементам в диапазоне от begin до end.
+// На вход функции f будет указатель на элемент.
+func ForEachPtr[T any](begin interfaces.ForwardIterator[T], end interfaces.Iterator, f forEachPtrFunc[T]) {
+	for it := begin; !it.Equals(end); it.Next() {
+		f(it.Ptr())
 	}
 }
