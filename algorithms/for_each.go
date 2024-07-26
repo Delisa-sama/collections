bc97f3e54f1cceb11005753ac36c9f1d91512e6f
Egor Dubin <delisa.sama@gmail.com>
2024-07-26 17:08:48 +0300

    Исправить ForEach и ForEachPtr, чтобы они принимали ValueIterator и PointerIterator соответственно.

--- algorithms/for_each.go
+++ algorithms/for_each.go
@@ -9,7 +9,7 @@ type forEachFunc[T any] func(T)
 
 // ForEach применяет функцию f ко всем элементам в диапазоне от begin до end.
 // На вход функции f будет копия элемента.
-func ForEach[T any](begin interfaces.ForwardIterator[T], end interfaces.Iterator, f forEachFunc[T]) {
+func ForEach[T any](begin interfaces.ValueIterator[T], end interfaces.Iterator, f forEachFunc[T]) {
 	for it := begin; !it.Equals(end); it.Next() {
 		f(it.Value())
 	}
@@ -20,7 +20,7 @@ type forEachPtrFunc[T any] func(*T)
 
 // ForEachPtr применяет функцию f ко всем элементам в диапазоне от begin до end.
 // На вход функции f будет указатель на элемент.
-func ForEachPtr[T any](begin interfaces.ForwardIterator[T], end interfaces.Iterator, f forEachPtrFunc[T]) {
+func ForEachPtr[T any](begin interfaces.PointerIterator[T], end interfaces.Iterator, f forEachPtrFunc[T]) {
 	for it := begin; !it.Equals(end); it.Next() {
 		f(it.Ptr())
 	}
